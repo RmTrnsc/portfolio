a87a1697eaddfdac3ea00613e665360752b9426b
romain <40247862+RmTrnsc@users.noreply.github.com>
2025-10-16 10:53:01 +0200

    style: New icons
    and change the path of folder

---
+++ src/app/ui/icons/arrowRight.tsx
@@ -0,0 +1,5 @@
+import { ArrowRightIcon } from "@heroicons/react/16/solid";
+
+export default function ArrowRight() {
+  return <ArrowRightIcon className="size-6 text-white" />;
+}
---
+++ src/app/ui/icons/arrowUpRight.tsx
@@ -0,0 +1,5 @@
+import { ArrowUpRightIcon } from "@heroicons/react/16/solid";
+
+export default function ArrowUpRight() {
+  return <ArrowUpRightIcon className="size-6 text-white" />;
+}
