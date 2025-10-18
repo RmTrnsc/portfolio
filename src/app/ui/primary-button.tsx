fc9a9f3d44fc3d18c7814aee06eeaee4c9b59dad
romain <40247862+RmTrnsc@users.noreply.github.com>
2025-10-18 18:11:12 +0200

    feat: Change the type

--- src/app/ui/primary-button.tsx
+++ src/app/ui/primary-button.tsx
@@ -2,11 +2,18 @@ import { Button } from "@headlessui/react";
 
 interface PrimaryButtonProps {
   children: React.ReactNode;
+  type?: 'button' | 'submit' | 'reset';
 }
 
-export default function PrimaryButton({ children }: PrimaryButtonProps) {
+export default function PrimaryButton({ 
+  children, 
+  type = 'button' 
+}: PrimaryButtonProps) {
   return (
-    <Button className="text-[#fffffb] bg-[var(--btn-bg-primary)] mx-auto py-1 px-2">
+    <Button 
+      type={type}
+      className="text-[#fffffb] bg-[var(--btn-bg-primary)] mx-auto py-1 px-2"
+    >
       {children}
     </Button>
   );
