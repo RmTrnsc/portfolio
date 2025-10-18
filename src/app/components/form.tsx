c29d6cd6dd4fe634ed050659af5e2b17f568421e
romain <40247862+RmTrnsc@users.noreply.github.com>
2025-10-18 18:10:38 +0200

    feat: new page
    - Creation of the new contact page
    - Creation of the new form component

---
+++ src/app/components/form.tsx
@@ -0,0 +1,141 @@
+"use client";
+
+import {
+  Controller,
+  FieldValues,
+  Path,
+  Resolver,
+  useForm,
+} from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Field,
+  Fieldset,
+  Input,
+  Label,
+  Legend,
+  Select,
+  Textarea,
+} from "@headlessui/react";
+import { EnvelopeIcon } from "@heroicons/react/16/solid";
+import PrimaryButton from "app/ui/primary-button";
+import z from "zod";
+
+export interface FormField<T> {
+  type: "text" | "email" | "tel" | "select" | "textarea";
+  label: string;
+  name: Path<T>;
+  options?: string[];
+  show?: boolean;
+  validation: {
+    required?: boolean;
+    errorMessage?: string;
+  };
+}
+
+interface FormProps<T extends FieldValues> {
+  title: string;
+  description: string;
+  fields: FormField<T>[];
+  onSubmit: (data: T) => void;
+  schema: z.ZodType<T, any, any>;
+  submitText?: string;
+}
+
+export default function Form<T extends FieldValues>({
+  title,
+  description,
+  fields,
+  onSubmit,
+  schema,
+  submitText = "Envoyer",
+}: FormProps<T>) {
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<T>({
+    resolver: zodResolver(schema) as Resolver<T>,
+  });
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Fieldset className="grid gap-4">
+        <Legend className="grid gap-2">
+          <h1 className="indent-4">{title}</h1>
+          <p>{description}</p>
+        </Legend>
+        {fields
+          .filter((field) => field.show)
+          .map((field, index) => (
+            <Field key={index} className="grid gap-2">
+              <Label htmlFor={field.name}>{field.label}</Label>
+              {field.type === "select" ? (
+                <Controller
+                  name={field.name}
+                  control={control}
+                  defaultValue={"" as any}
+                  render={({ field: { onChange, value } }) => (
+                    <Select
+                      name={field.name}
+                      required={field.validation.required}
+                      onChange={onChange}
+                      value={value || ""}
+                      className="border p-2"
+                    >
+                      <option value="">Sélectionnez un choix</option>
+                      {field.options?.map((option, idx) => (
+                        <option key={idx} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </Select>
+                  )}
+                ></Controller>
+              ) : field.type === "textarea" ? (
+                <Controller
+                  name={field.name}
+                  control={control}
+                  render={({ field: { onChange, value } }) => (
+                    <Textarea
+                      name={field.name}
+                      required={field.validation.required}
+                      onChange={onChange}
+                      value={value || ""}
+                      className="border p-2"
+                    />
+                  )}
+                />
+              ) : (
+                <Controller
+                  name={field.name}
+                  control={control}
+                  render={({ field: { onChange, value } }) => (
+                    <Input
+                      type={field.type}
+                      name={field.name}
+                      required={field.validation.required}
+                      onChange={onChange}
+                      value={value || ""}
+                      className="border p-2"
+                    />
+                  )}
+                />
+              )}
+              {errors[field.name] && (
+                <p className="text-red-600">
+                  {errors[field.name]?.message?.toString() ??
+                    "Ce champ est invalide."}
+                </p>
+              )}
+            </Field>
+          ))}
+        <PrimaryButton type="submit">
+          <EnvelopeIcon className="size-5 text-white" />
+          {submitText}
+        </PrimaryButton>
+      </Fieldset>
+    </form>
+  );
+}
--- src/app/dashboard/contact/page.tsx
+++ src/app/dashboard/contact/page.tsx
@@ -1,7 +1,92 @@
+"use client";
+import Form, { FormField } from "app/components/form";
+import { z } from "zod";
+
+const contactSchema = z.object({
+  lastname_name: z
+    .string()
+    .min(2, {
+      message: "Veuillez entrer un nom valide (au moins 2 caractères).",
+    })
+    .regex(/^[A-Za-zàâäéèêëîïôöùûüç\-\s']+$/, {
+      message:
+        "Format de nom invalide (lettres, tirets, espaces et apostrophes uniquement).",
+    }),
+  email: z.email({ message: "Veuillez entrer une adresse email valide." }),
+  phone: z.string().regex(/^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/, {
+    message: "Veuillez entrer un numéro de téléphone français valide.",
+  }),
+  project_type: z
+    .string()
+    .min(1, { message: "Veuillez sélectionner un type de projet." }),
+  project_description: z
+    .string()
+    .refine((val) => val === "" || val.length >= 10, {
+      message: "La description doit faire au moins 10 caractères ou être vide.",
+    })
+    .optional(),
+});
+
+type ContactFormData = z.infer<typeof contactSchema>;
+
 export default function Contact() {
+  const handleSubmit = async (data: ContactFormData) => {
+    console.log("Données soumises :", data);
+  };
+
+  const fields: FormField<ContactFormData>[] = [
+    {
+      type: "text",
+      label: "Nom et prénom",
+      name: "lastname_name",
+      show: true,
+      validation: { required: true },
+    },
+    {
+      type: "email",
+      label: "Email",
+      name: "email",
+      show: true,
+      validation: { required: true },
+    },
+    {
+      type: "tel",
+      label: "Téléphone",
+      name: "phone",
+      show: true,
+      validation: { required: true },
+    },
+    {
+      type: "select",
+      label: "Type de projet",
+      name: "project_type",
+      show: true,
+      options: [
+        "Nouveau projet",
+        "Évolution de projet",
+        "Demande d’information",
+        "Autre",
+      ],
+      validation: { required: true },
+    },
+    {
+      type: "textarea",
+      label: "Description de votre projet",
+      name: "project_description",
+      show: true,
+      validation: { required: false },
+    },
+  ];
+
   return (
-    <main className="flex flex-col gap-20">
-      <h1 className="font-header">Me contacter</h1>
+    <main className="w-full max-w-lg mx-auto">
+      <Form<ContactFormData>
+        title="Un nouveau projet, une évolution de vos besoins ou tout simplement une question ?"
+        description="Alors ce formulaire est pour vous, je vous répondrai dans les plus brefs délais !"
+        fields={fields}
+        onSubmit={handleSubmit}
+        schema={contactSchema}
+      />
     </main>
   );
 }
