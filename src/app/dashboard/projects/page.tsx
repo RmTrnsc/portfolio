5558de75d4859bd775eae29535433adaafbc8bfd
romain <40247862+RmTrnsc@users.noreply.github.com>
2025-10-24 11:35:34 +0200

    refactor: improvement displaying
    - Move the interface into new file, see this [commit {f4573e1}]
    - Change the displaying condition, see this [commit {d174cbb}]

--- src/app/dashboard/projects/page.tsx
+++ src/app/dashboard/projects/page.tsx
@@ -1,38 +1,27 @@
 import getProjects from "@/lib/getProjects";
+import { Project } from "app/types/Project";
 import PrimaryButton from "app/ui/primary-button";
 import Link from "next/link";
 
-interface Project {
-  id: number;
-  title: string;
-  presentation: string;
-  published: boolean;
-  slug: string;
-}
-
 export default async function Projects() {
   const projects = (await getProjects()) as Project[];
-
   return (
     <main className="flex flex-col gap-20">
       <div className="grid gap-8">
-        {projects.map(
-          (project) =>
-            project.published && (
-              <div key={project.id} className="grid gap-2">
-                <h3 className="font-header text-2xl">{project.title}</h3>
-                <div className="grid px-4 gap-4">
-                  <p>{project.presentation}</p>
-                  <Link
-                    href={`/dashboard/projects/${project.slug}`}
-                    className="text-right"
-                  >
-                    <PrimaryButton>Voir le projet</PrimaryButton>
-                  </Link>
-                </div>
-              </div>
-            )
-        )}
+        {projects.map((project) => (
+          <div key={project.id} className="grid gap-2">
+            <h3 className="font-header text-2xl">{project.title}</h3>
+            <div className="grid px-4 gap-4">
+              <p>{project.presentation.slice(0,100)}...</p>
+              <Link
+                href={`/dashboard/projects/${project.slug}`}
+                className="mx-auto"
+              >
+                <PrimaryButton>Voir la suite</PrimaryButton>
+              </Link>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
