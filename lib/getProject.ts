d174cbbdb44f5b1de6586b6d7926fc41e5782fc5
romain <40247862+RmTrnsc@users.noreply.github.com>
2025-10-24 11:29:36 +0200

    perf: Improvement of queries
    - Adding display conditions
    - Selecting elements to display for query optimization

--- lib/getProject.ts
+++ lib/getProject.ts
@@ -3,7 +3,16 @@ import { PrismaClient } from "../generated/prisma";
 const prisma = new PrismaClient();
 
 export default async function getProject(slug: string) {
-  return await prisma.project.findFirst({
-    where: { slug: slug },
+  return await prisma.project.findUnique({
+    where: {
+      slug: slug,
+    },
+    select: {
+      title: true,
+      presentation: true,
+      url: true,
+      updatedAt: true,
+      pictures: true,
+    },
   });
 }
--- lib/getProjects.ts
+++ lib/getProjects.ts
@@ -3,5 +3,14 @@ import { PrismaClient } from "../generated/prisma";
 const prisma = new PrismaClient();
 
 export default async function getProjects() {
-  return await prisma.project.findMany();
+  return await prisma.project.findMany({
+    where: { published: true },
+    orderBy: [{ updatedAt: "desc" }],
+    include: {
+      pictures: {
+        take: 1,
+        orderBy: { id: "desc" },
+      },
+    },
+  });
 }
