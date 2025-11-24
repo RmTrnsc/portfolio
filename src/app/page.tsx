462901f26824db8b979248f60f2edf09a223c6a7
romain <40247862+RmTrnsc@users.noreply.github.com>
2025-11-24 14:02:23 +0100

    feat: Add new animation on the welcoming page with animejs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,37 @@ import PrimaryButton from "./ui/primary-button";
 import ArrowRight from "./ui/icons/arrowRight";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { animate, splitText, stagger } from "animejs";
 
 export default function Home() {
- 
   const router = useRouter();
 
   useEffect(() => {
+    const { words } = splitText(".welcome-content", {
+      words: { wrap: "clip" },
+    });
     
+    animate(words, {
+      opacity: [0, 1],
+      duration: 750,
+      easing: "ease-in-out",
+      delay: stagger(50),
+    });
+
+    animate(".welcome-link", {
+      opacity: [0, 1],
+      duration: 750,
+      x: ["-5rem", "0rem"],
+      easing: "ease-in-out",
+      delay: 2000,
+    });
   });
 
   const handleClick = () => {
-    
+    animate(".welcome-container", {
+      opacity: [1, 0],
+      duration: 500,
+    }); 
     setTimeout(() => {
       router.push("/dashboard");
     }, 500);
@@ -31,17 +51,15 @@ export default function Home() {
           à leurs besoins.
         </p>
       </div>
-      <div className="flex justify-center">
-        <div
-          aria-describedby="link to dashboard"
-          className="welcome-link flex items-center"
-          onClick={() => handleClick()}
-        >
-          <PrimaryButton>
-            <ArrowRight />
-            Commencer la visite
-          </PrimaryButton>
-        </div>
+      <div
+        aria-describedby="link to dashboard"
+        className="welcome-link flex items-center"
+        onClick={() => handleClick()}
+      >
+        <PrimaryButton>
+          <ArrowRight />
+          Commencer la visite
+        </PrimaryButton>
       </div>
     </main>
   );
