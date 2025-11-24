b3d462da657baa6b5ff2410e982123321b4634cf
romain <40247862+RmTrnsc@users.noreply.github.com>
2025-11-24 13:32:51 +0100

    feat: Add animation on the dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import ArrowRight from "app/ui/icons/arrowRight";
 import PrimaryButton from "app/ui/primary-button";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { waapi, stagger, animate } from "animejs";
 
 export default function Dashboard() {
+  const router = useRouter();
+
   const links = [
     { href: "/dashboard/presentation", label: "Tout savoir, ou presque" },
     {
@@ -11,20 +17,40 @@ export default function Dashboard() {
     },
     { href: "/dashboard/contact", label: "On boit un café ?" },
   ];
+
+  useEffect(() => {
+    waapi.animate(".link", {
+      opacity: [0, 1],
+      duration: 750,
+      x: ["-5rem", "0rem"],
+      easing: "ease-in-out",
+      delay: stagger(500),
+    });
+  });
+
+  const handleClick = (href: string) => {
+    animate(".links-container", {
+      opacity: [1, 0],
+      duration: 500,
+    });
+    setTimeout(() => {
+      router.push(href);
+    }, 500);
+  };
   return (
-    <div className="flex flex-col gap-4 justify-center items-center">
+    <div className="links-container flex flex-col gap-4 justify-center items-center">
       {links.map((link) => (
-        <Link
+        <div
           key={link.href}
-          href={link.href}
           aria-description={`link to ${link.label}`}
-          className="flex items-center"
+          className="link flex items-center"
+          onClick={() => handleClick(link.href)}
         >
           <PrimaryButton key={link.href}>
             <ArrowRight />
             {link.label}
           </PrimaryButton>
-        </Link>
+        </div>
       ))}
     </div>
   );
