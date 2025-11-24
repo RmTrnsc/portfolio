1bc1326a941f2098126c255ee4f397098e766307
romain <40247862+RmTrnsc@users.noreply.github.com>
2025-11-24 13:37:12 +0100

    Removing the gsap library and its use

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,50 +2,19 @@
 
 import PrimaryButton from "./ui/primary-button";
 import ArrowRight from "./ui/icons/arrowRight";
-import { gsap } from "gsap";
-import { SplitText } from "gsap/all";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  gsap.registerPlugin(SplitText);
-  let tl = gsap.timeline({ defaults: { ease: "power1" } });
-  let container: any;
+ 
   const router = useRouter();
 
   useEffect(() => {
-    container = document.querySelector(".welcome-container");
-    gsap.set(container, { opacity: 1 });
-    let split;
-
-    SplitText.create(".welcome-content", {
-      type: "words",
-      wordsClass: "word",
-      autoSplit: true,
-      mask: "words",
-      onSplit: (data) => {
-        split = gsap.from(data.words, {
-          opacity: 0,
-          duration: 0.75,
-          stagger: 0.1,
-        });
-        return split;
-      },
-    });
-
-    tl.from(".welcome-link", {
-      opacity: 0,
-      duration: 0.5,
-      delay: 3.5,
-      transform: "translateX(-5rem)",
-    });
+    
   });
 
   const handleClick = () => {
-    tl.to(container, {
-      opacity: 0,
-      duration: 0.5,
-    });
+    
     setTimeout(() => {
       router.push("/dashboard");
     }, 500);
