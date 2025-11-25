769593a8f70ba61beafb91771709bdae1399b219
romain <40247862+RmTrnsc@users.noreply.github.com>
2025-11-25 12:01:20 +0100

    Refactor: Change the class of span for size of the screens displayign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     const { words } = splitText(".welcome-content", {
       words: { wrap: "clip" },
     });
-    
+
     animate(words, {
       opacity: [0, 1],
       duration: 750,
@@ -34,7 +34,7 @@ export default function Home() {
     animate(".welcome-container", {
       opacity: [1, 0],
       duration: 500,
-    }); 
+    });
     setTimeout(() => {
       router.push("/dashboard");
     }, 500);
@@ -44,7 +44,7 @@ export default function Home() {
     <main className="welcome-container flex flex-col gap-20 justify-center">
       <div className="text-justify">
         <p className="welcome-content md:w-1/2 md:mx-auto">
-          <span className="mx-2">Mon</span>
+          <span className="mx-2 md:mx-0 md:mr-2">Mon</span>
           travail est d'aider les entreprises, comme les particuliers, à créer
           ou améliorer leurs visibilités sur Internet, au travers d'applications
           mobiles, de sites web, en utilisant les technologies les plus adaptées
